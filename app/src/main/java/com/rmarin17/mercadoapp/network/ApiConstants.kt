e39bf070c5ed5998b122951f1d9bac8b7a8915b2
ricardo-marin-globant <ricardo.marin@globant.com>
2021-07-15 20:42:42 -0500

    Add Retrofit, and RX Java 3 libraries, add service calls and first steps. - Update build.gradle and dependencies.gradle to add retrofit, gson, hhttp and rx java versions. - Add ProductServices.kt to create the functions to calls the service endpoints. - Add ApiConstants.kt to extract into constants the service endpoints. - Add ProductResponseModel.kt to handle the response model. - Update AppModule.kt to inject retrofit builder and product service.

---
+++ app/src/main/java/com/rmarin17/mercadoapp/network/ApiConstants.kt
@@ -0,0 +1,11 @@
+package com.rmarin17.mercadoapp.network
+
+/**
+ * Constants of the api service.
+ */
+object ApiConstants {
+    const val URL_BASE = "https://fakestoreapi.com/"
+    const val GET_ALL_PRODUCTS = "products"
+    const val PRODUCT_ID = "productId"
+    const val GET_PRODUCT = "products/{$PRODUCT_ID}"
+}
